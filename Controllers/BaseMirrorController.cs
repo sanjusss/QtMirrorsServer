df30a893818c9669f0be0423116cf7014292954a
sanjusss <sanjusss@qq.com>
2020-03-25 18:45:28 +0800

    添加项目文件。

---
+++ Controllers/BaseMirrorController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QtMirrorsServer.Controllers
+{
+    public abstract class BaseMirrorController : Controller
+    {
+        public abstract string TargetUrl { get; }
+
+        [HttpGet("{*.}")]
+        public async Task<object> Get()
+        {
+            if (Request.Path.HasValue == false)
+            {
+                return NotFound();
+            }
+
+            int prefix = Request.RouteValues["controller"].ToString().Length + 1;
+            string path = TargetUrl + Request.Path.Value.Substring(prefix);
+            if (path.EndsWith(".xml"))
+            {
+                string content;
+                using (HttpClient client = new HttpClient())
+                {
+                    content = await client.GetStringAsync(path);
+                }
+
+                content = content.Replace("download.qt-project.org", Request.Host.Value);
+                return Content(content, "text/xml");
+            }
+            else
+            {
+#if DEBUG
+                return path;
+#else
+                return Redirect(path);
+#endif
+            }
+        }
+    }
+}
---
+++ Controllers/TencentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QtMirrorsServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TencentController : BaseMirrorController
+    {
+        public override string TargetUrl => @"https://mirrors.cloud.tencent.com/qt/";
+    }
+}
